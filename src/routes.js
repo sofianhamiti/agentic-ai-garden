0d82e88e3ec7affe27d714549eaf1854a793a8ad
SofianHamiti <sofian.hamiti.com>
2025-07-05 13:00:21 +0000

    fix router base path for GitHub Pages

--- src/routes.js
+++ src/routes.js
@@ -29,7 +29,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/website/'),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
